c342af43971e21bc561845385b4d81c7b191b28c
Wednesday <yensonyu@gmail.com>
2021-02-08 23:02:02 -0800

    create server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+// const { urlencoded } = require("express");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
